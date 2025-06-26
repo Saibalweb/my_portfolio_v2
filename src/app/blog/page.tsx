fcf6ca59ced4b63b66ef223bdbd6499101cf400b
Saibal kole <saibalkole@gmail.com>
2025-06-26 23:48:11 +0530

    fix blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,24 +4,7 @@ import React from 'react'
 const blogPage = () => {
   return (
     <div>
-      <Button variant={'default'} size={'lg'}>
-        Testing
-      </Button>
-      <Button variant={'destructive'}>
-        Testing
-      </Button>
-      <Button variant={'outline'}>
-        Testing
-      </Button>
-      <Button variant={'secondary'}>
-        Testing
-      </Button>
-      <Button variant={'ghost'}>
-        Testing
-      </Button>
-      <Button variant={'link'}>
-        Testing
-      </Button>
+      This is blog page
     </div>
   )
 }
