9b67f84b2f213a3ae54f6f713f5c0d5e8cb0fe81
Saibal kole <saibalkole@gmail.com>
2025-07-15 23:31:28 +0530

    changes main website name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,11 @@ import SocialComponent from "./SocialComponent";
 
 export default function Navbar() {
   const { theme } = useTheme();
-  console.log("theme", theme);
   return (
     <header className="sticky top-0 z-50 w-full border-b dark:border-gray-800 bg-background/75">
       <div className="container mx-auto flex h-16 max-w-6xl items-center justify-between px-4 md:px-6">
-        <Link href="#" className="flex items-center gap-2" prefetch={false}>
-          <MountainIcon className="h-6 w-6" />
-          <span className="sr-only">Acme Inc</span>
+        <Link href="#" className="flex items-center justify-center gap-2" prefetch={false}>
+          <span className="text-foreground text-2xl">Saibal Kole</span>
         </Link>
         <nav className="hidden items-center gap-10 text-sm font-medium md:flex">
           <Link
