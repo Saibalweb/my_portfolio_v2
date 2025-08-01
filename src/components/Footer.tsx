8f5f95150b803b4c56f780368a0d560c5452709b
Saibal kole <saibalkole@gmail.com>
2025-08-01 23:52:42 +0530

    refactor: Update footer component by removing commented sections and enhancing typography for headings

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,6 @@ const Footer = () => {
   return (
     <footer className="mx-auto mt-12 px-12 md:px-24 2xl:px-48 ">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12 xl:max-w-[1560px] mx-auto">
-        {/* Quick Links */}
         <div>
           <h3 className="font-medium text-foreground text-3xl font-['Inter',Helvetica] mb-6">
             Quick Links
@@ -27,7 +26,6 @@ const Footer = () => {
           </ul>
         </div>
 
-        {/* Contact Me */}
         <div className="flex flex-col md:items-center my-8 md:my-0">
           <h3 className="font-medium text-foreground text-3xl font-['Inter',Helvetica] mb-6">
             Contact Me
@@ -40,7 +38,6 @@ const Footer = () => {
           </p>
         </div>
 
-        {/* Follow Me */}
         <SocialComponent className="md:items-end"/>
       </div>
 
